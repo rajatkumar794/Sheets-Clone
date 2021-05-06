cd04658a220c852373a3fd5182df1824b47b82ef
RajatKumar <rajatrkpuram@gmail.com>
2021-05-06 20:36:14 +0530

    updates

--- init.js
+++ init.js
@@ -1,13 +1,35 @@
 let cellsContent = document.querySelector(".cells-content")
 
-let cells = ""
+
+//top left cell
+let cellsContentHtml = `<div class="top-left-cell"></div>`
+
+//top row
+cellsContentHtml+= `<div class="top-row">`
+for(let i=0; i<26; ++i){
+    cellsContentHtml+= `<div class="top-row-cell">${String.fromCharCode(65+i)}</div>`
+}
+cellsContentHtml+= `</div>`
+
+
+//left column
+cellsContentHtml+= `<div class="left-column">`
+for(let i=0; i<100; i++){
+    cellsContentHtml+= `<div class="left-column-cell">${i+1}</div>`
+}
+cellsContentHtml+= `</div>`
+
+
+//cells
+cellsContentHtml += `<div class="cells">`
 for(let i=0; i<100; ++i)
-{   cells += `<div class="row">`;
+{   cellsContentHtml += `<div class="row">`;
     for(let j=0; j<26; ++j)
     {
-        cells+=`<div class="cell" contenteditable="true"></div>`
+        cellsContentHtml+=`<div class="cell" id="${i+j+1}"contenteditable="true"></div>`
     }
-    cells+=`</div>`
+    cellsContentHtml+=`</div>`
 }
+cellsContentHtml+= `</div>`
 
-cellsContent.innerHTML = cells
+cellsContent.innerHTML = cellsContentHtml
